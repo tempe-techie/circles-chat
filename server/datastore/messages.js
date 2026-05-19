0302df414cd97151a66c6077e6c773dc51b04ba2
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2026-05-19 22:06:19 +0200

    Update messages.js

--- server/datastore/messages.js
+++ server/datastore/messages.js
@@ -62,8 +62,7 @@ export async function deleteMessage(key) {
 }
 
 export async function listMessages({ limit = 10, beforeKey } = {}) {
-  //let query = datastore.createQuery(KIND).order('__key__', { descending: true });
-  let query = datastore.createQuery(KIND).order('__key__'); // TODO: revert to descending order after testing
+  let query = datastore.createQuery(KIND).order('__key__', { descending: true });
 
   if (beforeKey) {
     query = query.start(datastore.key([KIND, beforeKey]));
